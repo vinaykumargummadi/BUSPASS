43229d33c103c90d5b4a6925c5eb92d1cddce245
vinay kumar <56590923+vinaykumargummadi@users.noreply.github.com>
2021-07-05 12:52:57 +0530

    Home page completed UI

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -11,7 +11,7 @@ import Logout from './logout'
 // import { logoutUser } from "../FireBaseConfig";
 import '../assets/css/function.css'
 import '../assets/css/home.css'
-import Vector from '../assets/images/vector.jpg'
+import Vector from '../assets/images/landing.png'
 
 const Dashboard : React.FC = () => {
 
@@ -51,9 +51,24 @@ const Dashboard : React.FC = () => {
 
             <p className="username">Welcome &nbsp;<span>{username},</span>                
                 </p>
-            <img src={Vector} alt="vector" width="440"></img>
+            <img src={Vector} alt="vector" width="440" className="vector-img"></img>
             <div className="wrapper">
-                
+                <div className="text-section">
+                    <h5 className="dash-title"> <span id="hero-text">Get attached </span><br/> with <br/> <span id="hero-text">
+                        Virtual World</span></h5>
+                    <div  className="dash-desc">
+                        <h6 className="ins-title">Instructions</h6>
+                        <ul className="ins-desc">
+                            <li>Swipe right to access the menu</li>
+                            <li>Head to Apply page to apply the bus pass</li>
+                            <li>Head to QR page to Generate the Virtual Pass</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="copy-section">
+                    <hr id="hr"/>
+                    <p className="c-desc">&nbsp;&nbsp;&#169;&nbsp;&nbsp; 2021 Virtual Pass <br/> Terms of use | Policy | about | Contact</p>
+                </div>
             </div>
             {/* <IonLoading message="Logging out.." duration={0} isOpen={busy}></IonLoading> */}
             {/* <IonButton onClick={logout}>Logout</IonButton> */}
